d56b19fae1dc30ac64e8aa2798cebb9dcaf73c94
Steven Tan <steven.tan@bulletproof.net>
2020-05-09 14:28:49 +1000

    Added L4D2 server

---
+++ l4d2/server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+replace_config() {
+  sed -i "s/${1} .*/${1} \"${2}\"/g" "$SERVER/left4dead2/cfg/server.cfg"
+}
+
+replace_config "sm_cvar survivor_friendly_fire_factor_easy" "${L4D2_FRIENDLYFIRE}"
+replace_config "sm_cvar survivor_friendly_fire_factor_normal" "${L4D2_FRIENDLYFIRE}"
+replace_config "sm_cvar survivor_friendly_fire_factor_hard" "${L4D2_FRIENDLYFIRE}"
+replace_config "sm_cvar survivor_friendly_fire_factor_expert" "${L4D2_FRIENDLYFIRE}"
+
+replace_config "hostname" "${L4D2_HOSTNAME}"
+replace_config "sv_password" "${L4D2_PASSWORD}"
+replace_config "rcon_password" "${L4D2_RCON}"
+
+replace_config "sm_cvar mp_gamemode" "${L4D2_GAMEMODE}"
+
+./srcds_run -console -game left4dead2 -secure +maxplayers "${L4D2_PLAYERS}" +map "${L4D2_MAP}"
