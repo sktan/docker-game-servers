14d04f4f5e3d956e601cb3ac841d9c3f7e9466e8
Steven Tan <git@sktan.com>
2020-05-20 08:11:43 +0000

    testing of zonemod instllation when launching versus

--- l4d2/server.sh
+++ l4d2/server.sh
@@ -4,6 +4,12 @@ replace_config() {
   sed -i "s/${1} .*/${1} \"${2}\"/g" "$SERVER/left4dead2/cfg/server.cfg"
 }
 
+if [ "${L4D2_GAMEMODE}" == "versus" ]; then
+  cp -rf "$HOME/mods/zonemod/*" "$SERVER/left4dead2/"
+else
+  cp -rf "$HOME/mods/sourcemod_base/*" "$SERVER/left4dead2/"
+fi
+
 replace_config "sm_cvar survivor_friendly_fire_factor_easy" "${L4D2_FRIENDLYFIRE}"
 replace_config "sm_cvar survivor_friendly_fire_factor_normal" "${L4D2_FRIENDLYFIRE}"
 replace_config "sm_cvar survivor_friendly_fire_factor_hard" "${L4D2_FRIENDLYFIRE}"
