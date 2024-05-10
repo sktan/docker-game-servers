43903881660b28a8774e5c6d1cefcdae22c398b9
Steven Tan <git@sktan.com>
2024-05-10 22:51:18 +1000

    auto-commit mods folder

--- dontstarvetogether/dst.sh
+++ dontstarvetogether/dst.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+DIR_MODS_SYS="/home/steam/dontstarvetogether/mods"
+DIR_MODS_USR="/home/steam/.klei/DoNotStarveTogether/MyDediServer/mods"
+
+if [[ -L "${DIR_MODS_SYS}" ]]; then
+  rm -rf "${DIR_MODS_SYS}"
+  cp -r "${DIR_MODS_USR}" "${DIR_MODS_SYS}"
+fi
+
+if [ ! -d "${DIR_MODS_USR}" ]; then
+  mkdir -p "${DIR_MODS_USR}"
+  cp -r "${DIR_MODS_SYS}" "${DIR_MODS_USR}"
+fi
+
+rm -rf "${DIR_MODS_SYS}"
+ln -s "${DIR_MODS_USR}" "${DIR_MODS_SYS}"
+
 run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
 run_shared+=(-console)
 run_shared+=(-cluster "MyDediServer")
